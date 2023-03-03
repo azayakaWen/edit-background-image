12273ac194be02cf006dee2bad6d1fee7e1b5366
Ew-amoeba <emelie.wensberg@amoeba.se>
2023-03-03 15:15:54 +0100

    Commit first code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
 
 function App() {
+  const [backgroundImage, setBackgroundImage] = useState("")
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0]
+    const reader = new FileReader()
+
+    reader.onload = (event) => {
+      setBackgroundImage(event.target.result)
+    }
+
+    reader.readAsDataURL(file)
+  }
+
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+    height: "100vh",
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={backgroundStyle}>
+      <div className="header-box">
+        <h1>Add background!</h1>
+      </div>
+      <div className="upload">
+        <input type="file" onChange={handleImageChange} />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+// import React, { useState } from "react"
+
+// function App() {
+//   const [imageUrl, setImageUrl] = useState("")
+
+//   const handleChange = (e) => {
+//     const file = e.target.files[0]
+//     const reader = new FileReader()
+
+//     reader.onloadend = () => {
+//       setImageUrl(reader.result)
+//     }
+
+//     if (file) {
+//       reader.readAsDataURL(file)
+//     }
+//   }
+
+//   return (
+//     <div
+//       style={{
+//         backgroundImage: `url(${imageUrl})`,
+//         backgroundSize: "cover",
+//         backgroundPosition: "center",
+//         width: "100vw",
+//         height: "100vh",
+//       }}
+//     >
+//       <input type="file" onChange={handleChange} />
+//     </div>
+//   )
+// }
+
+// export default App
